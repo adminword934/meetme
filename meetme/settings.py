1cd16f09f18ad35d9106f70c4685057e1509827e
adminword934 <adminword934@gmail.com>
2026-07-31 10:58:14 -0700

    Adding Cloudinary SDK

--- meetme/settings.py
+++ meetme/settings.py
@@ -32,6 +32,13 @@ DEBUG = config("DEBUG", cast=bool)
 ALLOWED_HOSTS = ["*"]
 
 
+#cloudinary
+
+cloudinary.config(
+    cloud_name=config("CLOUDINARY_CLOUD_NAME"),
+    api_key=config("CLOUDINARY_API_KEY"),
+    api_secret=config("CLOUDINARY_API_SECRET"),
+)
 
 
 # Application definition
